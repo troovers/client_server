d8bf328e984d9fac717201e57191b68cbeaa32f1
Thomas Roovers <Thomas Roovers>
2016-04-26 14:19:51 +0200

    Updated project so that the server returns the sentence, in uppercase

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -30,13 +30,15 @@ public class Server {
 
             System.out.println("CLIENT: "+clientSentence);
 
-            if(clientSentence.equals("Hello!")) {
+            /*if(clientSentence.equals("Hello!")) {
                 outSentence =  "Well hello, how are you?\n";
             } else if(clientSentence.equals("I'm fine!")) {
                 outSentence =  "That's great!\n";
             } else {
                 outSentence = "I'm really stupid, I don't understand your message.. Try 'Hello!'?\n";
-            }
+            }*/
+
+            outSentence = clientSentence.toUpperCase();
 
             outToClient.writeBytes(outSentence);
 
